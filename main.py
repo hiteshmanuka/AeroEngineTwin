b315fe9d6332800511e092b8ff483934711c1f0c
prathamd69 <prathamd2710@gmail.com>
2026-09-14 00:22:35 +0530

    Refactor main function to integrate simulation ingestion and downsampling processes

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from src.services import ingest_simulation_csv
+from src.utils import *
+from src.services import *
 from pathlib import Path
 
-path = Path(r"data\run_1_cc554525120.csv")
-ingest_simulation_csv(path)
+def main():
+    sim_config_path = r"config\sim_config.yaml"
+    artifacts_config_path = r"config\artifacts.yaml"
+
+    simbox = SimConfigManager(sim_config_path)
+    artifactsbox = ArtifactConfigManager(artifacts_config_path).sim_paths
+
+    rawpath = Path(artifactsbox.raw_sim)
+    valpath = Path(artifactsbox.validated_sim)
+    downpath = Path(artifactsbox.downsampled_sim)
+
+    buckets = int(simbox.downsampling.buckets)
+
+    s = SimulationIngestor(rawpath, valpath)
+    s.ingest_and_save()
+
+    d = SimulationDownsampler(buckets, valpath, downpath)
+    d.downsample_m4()
+    return
+
+if __name__ == "__main__":
+    main()
