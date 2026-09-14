eb529a6ece2cdfaf236b951875e9c7937cdabe43
prathamd69 <prathamd2710@gmail.com>
2026-09-15 05:05:42 +0530

    Refactor configuration handling: Replace hardcoded values with environment variables for REGION_NAME and encoding settings

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import os
-from datetime import timedelta
-
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
@@ -22,6 +20,14 @@ SEAWEED_ENDPOINT = os.environ.get("SEAWEED_ENDPOINT")
 SIM_BUCKET = os.environ.get("RAW_SIM_BUCKET")
 AWS_ACCESS_KEY = os.environ.get("SEAWEED_AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.environ.get("SEAWEED_AWS_SECRET_KEY")
-REGION_NAME= "us-east-1"
+REGION_NAME= os.environ.get("REGION_NAME")
 
-ENCODING="UTF-16"
+#ingestion-encoding
+ENCODING=os.environ.get("ENCODING")
+
+#downsampling
+MIN_BUCKETS=int(os.environ.get("MIN_BUCKETS"))
+MAX_BUCKETS=int(os.environ.get("MAX_BUCKETS"))
+DEFAULT_BUCKETS=int(os.environ.get("DEFAULT_BUCKETS"))
+DEFAULT_START=int(os.environ.get("DEFAULT_START"))
+DEFAULT_END=int(os.environ.get("DEFAULT_END"))
